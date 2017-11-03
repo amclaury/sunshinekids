368406be2fdd47abcd500c842c7d5d498a0650a3
javievazq <javievazq@knights.ucf.edu>
2017-11-03 13:38:30 -0400

    annual reports
    annual reports pdf

--- annualreports.php
+++ annualreports.php
@@ -49,11 +49,27 @@
 		   <div class ="container">
       <div class="row center">
         <div class="col s12">
-          <div class="card center">
-            <div class="card-image">
-              <img src="img/annualpdf.jpg">
-            </div>
-          </div>
+        
+  <ul class="collapsible" data-collapsible="accordion">
+    <li>
+      <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+      <div class="collapsible-body"> 
+      <iframe src="http://sulley.cah.ucf.edu/~dig4104c_group04/pdf/Annual_Report_2016.pdf" style="width:800px; height:600px;" frameborder="0"></iframe>
+      </div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+      <div class="collapsible-body">
+        <iframe src="http://sulley.cah.ucf.edu/~dig4104c_group04/pdf/Annual_Report_2015.pdf" style="width:800px; height:600px;" frameborder="0"></iframe>
+      </div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+      <div class="collapsible-body">
+        <iframe src="http://sulley.cah.ucf.edu/~dig4104c_group04/pdf/Annual_Report_2014.pdf" style="width:800px; height:600px;" frameborder="0"></iframe>
+      </div>
+    </li>
+  </ul>
         </div>
 		</div>
 	</div>	
@@ -66,6 +82,9 @@
 
 
     <script>
+     $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
    $(document).ready(function(){
      $('.parallax').parallax();
    });
@@ -81,5 +100,106 @@ function autoplay() {
     $('.carousel').carousel('next');
     setTimeout(autoplay, 8000);
 }
+
+
+    </script>
+      <script src="https://mozilla.github.io/pdf.js/build/pdf.js"></script>
+      <script>
+       var url = 'http://sulley.cah.ucf.edu/~dig4104c_group04/pdf/Annual_Report_2016.pdf';
+
+    //
+    // Disable workers to avoid yet another cross-origin issue (workers need the URL of
+    // the script to be loaded, and currently do not allow cross-origin scripts)
+    //
+    PDFJS.disableWorker = true;
+
+    var pdfDoc = null,
+        pageNum = 1,
+        pageRendering = false,
+        pageNumPending = null,
+        scale = 1.5,
+        canvas = document.getElementById('the-canvas'),
+        ctx = canvas.getContext('2d');
+
+    /**
+     * Get page info from document, resize canvas accordingly, and render page.
+     * @param num Page number.
+     */
+    function renderPage(num) {
+      pageRendering = true;
+      // Using promise to fetch the page
+      pdfDoc.getPage(num).then(function(page) {
+        var viewport = page.getViewport(scale);
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        // Render PDF page into canvas context
+        var renderContext = {
+          canvasContext: ctx,
+          viewport: viewport
+        };
+        var renderTask = page.render(renderContext);
+        
+        // Wait for rendering to finish
+        renderTask.promise.then(function () {
+          pageRendering = false;
+          if (pageNumPending !== null) {
+            // New page rendering is pending
+            renderPage(pageNumPending);
+            pageNumPending = null;
+          }
+        });
+      });
+
+      // Update page counters
+      document.getElementById('page_num').textContent = pageNum;
+    }
+    
+    /**
+     * If another page rendering in progress, waits until the rendering is
+     * finised. Otherwise, executes rendering immediately.
+     */
+    function queueRenderPage(num) {
+      if (pageRendering) {
+        pageNumPending = num;
+      } else {
+        renderPage(num);
+      }
+    }
+
+    /**
+     * Displays previous page.
+     */
+    function onPrevPage() {
+      if (pageNum <= 1) {
+        return;
+      }
+      pageNum--;
+      queueRenderPage(pageNum);
+    }
+    document.getElementById('prev').addEventListener('click', onPrevPage);
+
+    /**
+     * Displays next page.
+     */
+    function onNextPage() {
+      if (pageNum >= pdfDoc.numPages) {
+        return;
+      }
+      pageNum++;
+      queueRenderPage(pageNum);
+    }
+    document.getElementById('next').addEventListener('click', onNextPage);
+
+    /**
+     * Asynchronously downloads PDF.
+     */
+    PDFJS.getDocument(url).then(function (pdfDoc_) {
+      pdfDoc = pdfDoc_;
+      document.getElementById('page_count').textContent = pdfDoc.numPages;
+
+      // Initial/first page rendering
+      renderPage(pageNum);
+    });
     </script>
   </html>
